acb2f3dc1c3c70558eab85ae88803f97494ba2c5
GJMcGowan <gemcgow@gmail.com>
2015-06-10 16:16:52 +0100

    Making individual # and class tests pass

--- Answer.js
+++ Answer.js
@@ -1,6 +1,8 @@
 var $ = function (selector) {
   var elements = [];
 
+  // Is it trying to select divs?
+  // Super-duper hacky.
   if(selector === "div") {
     var div = document.getElementsByTagName("div")
     for (var i = div.length - 1; i >= 0; i--) {
@@ -8,9 +10,18 @@ var $ = function (selector) {
     };
   };
 
-  // if(selector === "#") {
-  //   document.getElementById(selector)
-  // }
+  // Is it trying to select by ID?
+  if(selector.charAt(0) === "#") {
+    elements.push(document.getElementById(selector.substring(1)))
+  };
+
+  // Is it trying to select by class?
+  if(selector.charAt(0) === ".") {
+    var classes = document.getElementsByClassName(selector.substring(1))
+    for (var i = classes.length - 1; i >= 0; i--) {
+      elements.push(classes[i])
+    };
+  }
 
   return elements;
 }
