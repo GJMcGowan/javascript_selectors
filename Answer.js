abac6d0a35cfe96c94b02cbf77d88cf6eed99284
GJMcGowan <gemcgow@gmail.com>
2015-06-10 13:09:10 +0100

    Initial commit

---
+++ Answer.js
@@ -0,0 +1,13 @@
+var $ = function (selector) {
+  var elements = [];
+
+  var div = function() {
+    
+  }
+
+  ////////////////////
+  // Your code here //
+  ////////////////////
+
+  return elements;
+}
