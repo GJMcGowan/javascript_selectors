fe31ed33810ff5ab959c2e5b44f0faca9c15d95a
GJMcGowan <gemcgow@gmail.com>
2015-06-14 22:00:28 +0100

    Tests all passing, however code quite hacky - needs a lot of refactoring

--- Answer.js
+++ Answer.js
@@ -1,22 +1,25 @@
 var $ = function (selector) {
   var elements = [];
-  var validCSS = ["div", "input", "img", "DIV", "INPUT", "IMG"];
+  var validCSS = ["div", "input", "img"];
   words = [];
 
   // Splits id's and classes away from each other, so they can be dealt with separetly.
   var parser = function(selector) {
     newSelector = selector.replace("#", ",#").replace(".", ",.");
     words = newSelector.split(",");
+    adder();
   };
 
   // Applies functions to each array item, based on whether they are ids, classes or whatever.
   var adder = function() {
     for(var i = words.length - 1; i >= 0; i--) {
       if(words[i].charAt(0) === "#"){
-        getId(words[i])
+        getId(words[i]);
+        // Not affecting tests
         culler(words[i]);
       } else if(words[i].charAt(0) === ".") {
-        getClass(words[i])
+        getClass(words[i]);
+        // Not affecting tests
         culler(words[i]);
       } else if(validCSS.indexOf(words[i]) > -1) {
         getCSS(words[i]);
@@ -29,11 +32,13 @@ var $ = function (selector) {
 
   var deleter = function(element, word) {
     if(word.charAt(0) === "#"){
+      console.log("Maybe running with " + word + " and "+ element.id)
       if(element.id !== word.substring(1)) {
+        console.log("Running with " + word + " and "+ element.id)
         elements.splice(elements.indexOf(element), 1);
       };
     } else if(word.charAt(0) === ".") {
-      var array = element.className.split(" ")
+      var array = element.className.split(" ");
       if(array.indexOf(word.substring(1)) <= -1) {
         elements.splice(elements.indexOf(element), 1);
       };
@@ -46,24 +51,14 @@ var $ = function (selector) {
     };
   };
 
-  // .id, .className, .tagName are very important now...
-  // var array = [1, 5, 7]
-  // var index = array.indexof(5)
-  // if (index > -1 ) {
-      // array.splice(index, 1)
-  // }
-
-  // Run this after each iteration through, it checks whether items in the array 
-  // are valid according to this criteria as well, otherwise takes them out.
   var culler = function(word) {
     for(var i = elements.length - 1; i >= 0; i--) {
-      // for the element at this position, is the word true of it?
-      // So is it an image and of the right class
-      // It is of the right class, but does it have the right id etc.
       elementzzz = elements;
       deleter(elements[i], word);
     };
   };
+
+  // Helpers //
 
   var getClass = function(className) {
     var classes = document.getElementsByClassName(className.substring(1));
@@ -78,7 +73,7 @@ var $ = function (selector) {
     element = (document.getElementById(id.substring(1)))
     if(elements.indexOf(element) <= -1) {
       elements.push(element);      
-    }
+    };
   };
 
   var getCSS = function(css) {
@@ -91,15 +86,9 @@ var $ = function (selector) {
   };
 
   parser(selector);
-  adder();
 
-  // // Is it trying to select images?
-  // if(selector.includes("img")) {
-  //   var images = document.images;
-  //   for (var i = images.length - 1; i >= 0; i--) {
-  //     elements.push(images[i]);
-  //   };
-  // };
+  // Required as for some reason the iterator cuts out on the last run through.
+  culler(words[words.length - 1])
 
   return elements;
 };
