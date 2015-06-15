1ab632709cdea19f098dd2cd1ec9119d5bc6a951
GJMcGowan <gemcgow@gmail.com>
2015-06-15 16:25:12 +0100

    A lot of tidying up

--- Answer.js
+++ Answer.js
@@ -3,10 +3,9 @@ var $ = function (selector) {
   var validCSS = ["div", "input", "img"];
   words = [];
 
-  // Splits id's and classes away from each other, so they can be dealt with separetly.
+  // Splits id's and classes away from each other, so they can be dealt with separately.
   var parser = function(selector) {
-    newSelector = selector.replace("#", ",#").replace(".", ",.");
-    words = newSelector.split(",");
+    words = selector.replace("#", ",#").replace(".", ",.").split(",");
     adder();
   };
 
@@ -15,50 +14,58 @@ var $ = function (selector) {
     for(var i = words.length - 1; i >= 0; i--) {
       if(words[i].charAt(0) === "#"){
         getId(words[i]);
-        // Not affecting tests
-        culler(words[i]);
       } else if(words[i].charAt(0) === ".") {
         getClass(words[i]);
-        // Not affecting tests
-        culler(words[i]);
       } else if(validCSS.indexOf(words[i]) > -1) {
         getCSS(words[i]);
-        culler(words[i]);
       } else {
         console.log("Nothing found");
       };
+      deleter(words[i]);
     };
   };
 
-  var deleter = function(element, word) {
-    if(word.charAt(0) === "#"){
-      console.log("Maybe running with " + word + " and "+ element.id)
-      if(element.id !== word.substring(1)) {
-        console.log("Running with " + word + " and "+ element.id)
-        elements.splice(elements.indexOf(element), 1);
-      };
-    } else if(word.charAt(0) === ".") {
-      var array = element.className.split(" ");
-      if(array.indexOf(word.substring(1)) <= -1) {
-        elements.splice(elements.indexOf(element), 1);
-      };
-    } else if(validCSS.indexOf(word) > -1) {
-      if(element.tagName !== word.toUpperCase()) {
-        elements.splice(elements.indexOf(element), 1);        
-      };
-    } else {
-      console.log("Nothing");
-    };
-  };
-
-  var culler = function(word) {
+  var deleter = function(word) {
     for(var i = elements.length - 1; i >= 0; i--) {
-      elementzzz = elements;
-      deleter(elements[i], word);
+      if(word.charAt(0) === "#"){
+        deleteByID(elements[i], word)
+      } else if(word.charAt(0) === ".") {
+        deleteByClass(elements[i], word)
+      } else if(validCSS.indexOf(word) > -1) {
+        deleteByCSS(elements[i], word)
+      } else {
+        console.log("Nothing");
+      };
     };
   };
+
+  // Either get rid of this, or remove more functionality into it
+  // var culler = function(word) {
+  //   for(var i = elements.length - 1; i >= 0; i--) {
+  //     deleter(elements[i], word);
+  //   };
+  // };
 
   // Helpers //
+
+  var deleteByID = function(element, id) {
+    if(element.id !== id.substring(1)) {
+      elements.splice(elements.indexOf(element), 1);
+    };
+  };
+
+  var deleteByClass = function(element, word) {
+    var array = element.className.split(" ");
+    if(array.indexOf(word.substring(1)) <= -1) {
+      elements.splice(elements.indexOf(element), 1);
+    };
+  };
+
+  var deleteByCSS = function(element, css) {
+    if(element.tagName !== css.toUpperCase()) {
+      elements.splice(elements.indexOf(element), 1);        
+    };
+  };
 
   var getClass = function(className) {
     var classes = document.getElementsByClassName(className.substring(1));
@@ -72,7 +79,7 @@ var $ = function (selector) {
   var getId = function(id) {
     element = (document.getElementById(id.substring(1)))
     if(elements.indexOf(element) <= -1) {
-      elements.push(element);      
+      elements.push(element);
     };
   };
 
@@ -81,14 +88,14 @@ var $ = function (selector) {
     for (var i = items.length - 1; i >= 0; i--) {
       if(elements.indexOf(items[i]) <= -1) {
         elements.push(items[i]);
-      }
+      };
     };   
   };
 
   parser(selector);
 
   // Required as for some reason the iterator cuts out on the last run through.
-  culler(words[words.length - 1])
+  deleter(words[words.length - 1]);
 
   return elements;
 };
